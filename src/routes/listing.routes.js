dda0054f2aa56cdcd4299f193602a49bd3ceeb0b
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-19 12:21:17 +0100

    updated the listing routes for the new controllers

--- src/routes/listing.routes.js
+++ src/routes/listing.routes.js
@@ -7,15 +7,23 @@ import {
     deleteListing,
     getListingFiltered,
     likeListing,
-    unlikeListing
+    unlikeListing,
+    updateDescription,
+    updateTips,
+    updateTitle,
+    updateLocation,
+    removeImage,
+    addImage,
+    updateTagsAndCategories,
 } from "../controllers/listing.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-//route for listings
+// route for listings (collection)
+// changed from `/all` to `/` for a standard RESTful collection endpoint
 router
-    .route("/all")
+    .route("/")
     .get(getListings)
     .post(upload.fields([{ name: "images", maxCount: 50 }]), createListing);
 
@@ -34,6 +42,29 @@ router
     .post(likeListing)
     .patch(unlikeListing);
 
+router
+    .route("/:id/description")
+    .patch(updateDescription);
 
+router
+    .route("/:id/tips")
+    .patch(updateTips);
+
+router
+    .route("/:id/title")
+    .patch(updateTitle);
+
+router
+    .route("/:id/location")
+    .patch(updateLocation);
+
+router
+    .route("/:id/images")
+    .post(upload.fields([{ name: "images", maxCount: 50 }]), addImage)
+    .delete(removeImage);
+
+router
+    .route("/:id/tags-categories")
+    .patch(updateTagsAndCategories);
 
 export default router;
